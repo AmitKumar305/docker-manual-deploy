6ef4a053763e11b71e8f4bb8ad56a3afd341bfdc
Amit Kumar <65401586+AmitKumar305@users.noreply.github.com>
2023-11-23 14:08:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('express')();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('<H1>Hello from HOME</H1>');
+    res.send('<H1>Hello from HOME 123</H1>');
 })
 
 app.listen(PORT, () => {
